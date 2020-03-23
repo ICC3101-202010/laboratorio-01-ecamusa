d431e45a75f64d1a27af0bdeb60d0c5e09ad241a
ecamusa <epcamus@miuandes.cl>
2020-03-23 14:43:34 -0300

    laboratorio1

---
+++ Laboratorio1_1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_1
+{
+    public class Persona
+    {
+        public string name;
+        public string lastname;
+        public Persona(string aName, string aLastname)
+        {
+            name = aName;
+            lastname = aLastname;
+
+            Console.WriteLine(name);
+            Console.WriteLine(lastname);
+
+        }
+        public int Lanzar()
+        {
+            Random rand = new Random();
+            int a = rand.Next(0, 3);
+
+            return a;
+
+        }
+    }
+}
---
+++ Laboratorio1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Bob", "Kunga");
+            Console.WriteLine(persona1.Lanzar());
+            Console.ReadLine();
+
+        }
+    }
+}
